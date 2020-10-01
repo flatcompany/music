4c95d91285a67281482ee49f148cca7fceb7cb3a
Андрей <58937468+flatcompany@users.noreply.github.com>
2020-10-01 19:01:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+ new fullpage('#fullpage', {
+  anchors: ['page1', 'page2'],
+  // sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+  responsiveHeight: 900
+});
+});
+
